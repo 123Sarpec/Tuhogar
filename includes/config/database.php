cdc7bc5c1df28ee294429524bd6aa7c29d2ce0e6
Wilmer Sarpec <sarpecwilmer7@gmail.com>
2024-09-04 19:49:13 -0500

    Primer commit: BIENESRAICESMVC

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,12 +1,11 @@
 
 <?php
 
-function conectarDB():mysqli
-{
-    $db =  new mysqli('localhost','root', 'root','crud_bienesraices');
-    if(!$db){
-        echo "Conexion Fallida";
-        exit;
-    }
-    return $db;
-}
+session_start();
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+$db_mysqli_connect("DB_HOST","DB_USER","DB_PASSWORD","DB_NAME","DB_PORT");
+?>
